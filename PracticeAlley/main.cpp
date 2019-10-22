c9ce4d84d04ca396dcd6b1bc218ffa2d60b2e42a
楊為守 <WeiShou_Yang@2001-b400-e3d1-b751-49d5-eea3-4fdf-4adc.emome-ip6.hinet.net>
2019-10-22 16:19:06 +0800

    Initial Commit

---
+++ PracticeAlley/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PracticeAlley
+//
+//  Created by 楊為守 on 2019/10/22.
+//  Copyright © 2019年 WeiShou_Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
